828021d87d1bed4cae896810779cd72c16eae466
Carlos Zuluaga Mora <carloszulmor@unisabana.edu.co>
2024-09-10 16:08:33 -0500

    Agrega clases de equivalencia

--- src/main/java/edu/unisabana/dyas/tdd/registry/Registry.java
+++ src/main/java/edu/unisabana/dyas/tdd/registry/Registry.java
@@ -1,8 +1,46 @@
 package edu.unisabana.dyas.tdd.registry;
 
+import java.util.ArrayList;
+
 public class Registry {
+
+    private ArrayList<Integer> ids = new ArrayList<Integer>();
+
     public RegisterResult registerVoter(Person p) {
-        // TODO Validate person and return real result.
-        return RegisterResult.VALID;
+        if (!p.isAlive()) {
+            return RegisterResult.DEAD;
+        } else if (this.ids.contains(p.getId())) {
+            return RegisterResult.DUPLICATED;
+        } else if (p.getAge() < 0 || p.getAge() > 122) { //Edad de la persona más vieja del mundo
+            return RegisterResult.INVALID_AGE;
+        } else if (p.getAge() < 18) {
+            return RegisterResult.UNDERAGE;
+        } else {
+            this.ids.add(p.getId());
+            return RegisterResult.VALID;
+        }
+
+
+    }
+
+    public String genderComprobation(Person p) {
+        try {
+            Gender result = p.getGender();
+            if (result != Gender.MALE && result != Gender.FEMALE && result != Gender.UNIDENTIFIED) {
+                return "El género no es válido";
+            }
+            return result.toString();
+        } catch (IllegalArgumentException e) {
+            return "El género no es válido";
+        }
+    }
+
+    public String nombreVacio(Person p){
+        String nombre = p.getName();
+        if(nombre.equals("")){
+            return "Este es un nombre inválido";
+        }else{
+            return "Nombre aceptado";
+        }
     }
 }
--- src/test/java/edu/unisabana/dyas/tdd/registry/RegistryTest.java
+++ src/test/java/edu/unisabana/dyas/tdd/registry/RegistryTest.java
@@ -1,16 +1,69 @@
 package edu.unisabana.dyas.tdd.registry;
 
 import org.testng.AssertJUnit;
+import org.testng.annotations.Test;
+
+import java.util.ArrayList;
+
+import static edu.unisabana.dyas.tdd.registry.Gender.valueOf;
 
 public class RegistryTest {
     private Registry registry = new Registry();
     private AssertJUnit Assert;
 
-    @org.testng.annotations.Test
-    public void validateRegistryResult() {
-        Person person = new Person();
+    /** Clase de equivalencia 1 (Válida):
+     * Identificar persona muerta
+     */
+    @Test
+    public void stayinAlive() { //Se espera que lance muerto
+        Person person = new Person("Pepe", 123, 200, Gender.MALE, false);
         RegisterResult result = registry.registerVoter(person);
-        Assert.assertEquals(RegisterResult.VALID, result);
+        Assert.assertEquals(RegisterResult.DEAD, result);
     }
-    // TODO Complete with more test cases
+
+    /** Clase de equivalencia 3 (No Válida):
+     * Subir personas repetidas y que no dé RegisterResult.DEAD
+     */
+    @Test
+    public void repetidas() { //No puede haber IDs repetidos
+        Person persona = new Person("Pepe", 123, 200, Gender.MALE, false);
+        Person persona1 = new Person("Sierra", 123, 200, Gender.MALE, false);
+        RegisterResult result = registry.registerVoter(persona);
+        RegisterResult result1 = registry.registerVoter(persona1);
+        AssertJUnit.assertNotSame(RegisterResult.VALID, result1);
+    }
+
+    /** Clase de equivalencia 3 (Válida):
+     * Identificar persona menor de edad
+     */
+    @Test
+    public void menorDeEdad() { //la persona no es mayor de edad
+        Person person = new Person("Pepe", 123, 10, Gender.MALE, true);
+        RegisterResult result = registry.registerVoter(person);
+        Assert.assertEquals(RegisterResult.UNDERAGE, result);
+    }
+
+    /** Clase de equivalencia 4 (No Válida):
+     * Identificar una edad negativa
+     */
+    @Test
+    public void edadInvalida() { //la edad es menor que 0
+        Person person = new Person("Pepe", 123, -1, Gender.MALE, true);
+        RegisterResult result = registry.registerVoter(person);
+        Assert.assertNotSame(RegisterResult.DEAD, result);
+    }
+
+    /** Clase de equivalencia 5 (No Válida):
+     * Identificar un nombre vacío
+     */
+    @Test
+    public void nombreVacio() { //el nombre es ""
+        Person person = new Person("", 123, 10, Gender.MALE, true);
+        Person person1 = new Person("Pepe", 456, 19, Gender.MALE, true);
+        String result = registry.nombreVacio(person);
+        String result1 = registry.nombreVacio(person1);
+        Assert.assertNotSame(RegisterResult.INVALID_AGE, result);
+        Assert.assertNotSame(RegisterResult.DEAD, result1);
+    }
+
 }
